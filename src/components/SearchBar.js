f30fb633ca4b02c7b7bbcc508841071b9856a582
bvasilop <bvasilop@gmail.com>
2019-08-18 20:48:07 -0700

    added project files for react unsplash pics app

---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    // const { term } = this.state;
+    // const { onFormSubmit } = this.onFormSubmit;
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="image-search" id="image-search">
+              Image Search
+            </label>
+            <input
+              id="image-search"
+              type="text"
+              placeholder="Search..."
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
