b12109761b8c7fa5244d1c1509fcbda2b7948494
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2019-12-30 20:41:00 -0800

    created .env for private api key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization:
-      'Client-ID 61d77b773ab1ee4df8113774c965eef2286fd4987145f4b6182a27da3d798474',
+    Authorization: API_KEY,
   },
 });
