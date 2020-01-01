c7e29a49084dbb1c70b108e3b3d9b83e0aef4986
Bill Vasilopoulos <billvasilopoulos@Bills-MBP-2.domain>
2019-12-31 17:07:03 -0800

    updated api key info

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
+const API_KEY = `Client-ID 61d77b773ab1ee4df8113774c965eef2286fd4987145f4b6182a27da3d798474`;
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
